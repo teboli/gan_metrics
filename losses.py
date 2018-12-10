6d4e3da762893ebb0e3fc14f3ca8a9a7743f60b0
teboli <thomas.eboli12@gmail.com>
2018-12-10 15:27:42 +0100

    add per_pixel_acc, per_class_acc and class_iou

---
+++ losses.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn.functional as F
+
+def per_pixel_acc(input, num_classes):
+    output = torch.zeros_like(input)
+    for c in range(num_classes):
+        output += input.eq(c * torch.ones_like(input)).float().div(input.numel())
+    return output.div(num_classes)
+
+
+def per_class_acc(input, target, threshold, num_classes):
+    output = torch.zeros_like(input)
+    for c in range(num_classes):
+        input_c  = input.eq(c * torch.zeros_like(input)).float()
+        target_c = target.eq(c * torch.zeros_like(target)).float()
+        tp = input_c * target_c
+        f = 1 - (input_c * target_c)
+        denom = (tp + f).sum()
+        denom[denom > 0] = 1
+        output += tp.sum().div()
+    return output.div(num_classes)
+
+
+def class_iou(input, target, num_classes):
+    output = torch.zeros_like(input)
+    for c in range(num_classes):
+        input_c  = input.eq(c * torch.zeros_like(input)).float()
+        target_c = target.eq(c * torch.zeros_like(output)).float()
+        inter = input_c * target_c
+        union = input_c + target_c - inter
+        output += inter.sum().div(union.sum())
+    return output.div(num_classes)
--- metrics.py
+++ metrics.py
@@ -1,6 +1,9 @@
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torchvision import models, transforms
+
+import numpy as np
 
 import losses, networks
 
@@ -20,8 +23,9 @@ class InceptionScore(nn.Module):
         assert(input.shape[1] == 3 and input.shape[2] == 299 and input.shape[3] == 299)
         self.model.eval()
 
-        n = input.shape
+        n = input.shape[0]
         n_batches = int(math.ceil(float(n) / float(self.batch_size)))
+
         probs = []
         for i in range(n_batches):
             start = i * self.batch_size
@@ -36,23 +40,48 @@ class InceptionScore(nn.Module):
             end   = (i+1) * n // self.splits
             probs_split = probs[start:end]
             p = p.mean(dim=0,keepdim=True).log()
-            kl = torch.nn.functional.kl_div(probs_split, p, reduction='none').sum(dim=1).mean()
+            kl = F.kl_div(probs_split, p, reduction='none').sum(dim=1).mean()
             scores.append(kl.exp().item())
 
         return np.mean(scores), np.std(scores)
 
 
 class FCNScore(nn.Module):
-    def __init__(self, state_dict=None):
+    def __init__(self, state_dict=None, batch_size=100, splits=10):
         self.model = networks.fcn_8s()
+        self.batch_size = batch_size
+        self.splits = splits
         if state_dict is not None:
             self.model.load_state_dict(state_dict)
 
     def forward(self, input, target):
         self.model.eval()
-        input_feat  = self.model(input).softmax(dim=1)
-        target_feat = self.model(target).softmax(dim=1)
-        return torch.nn.functional.kl_div(input_feat, target_feat).exp()
+
+        n = input.shape[0]
+        n_batches = int(math.ceil(float(n) / float(self.batch_size)))
+
+        labels = []
+        for i in range(n_batches):
+            start = i * self.batch_size
+            end   = min(i+1 * self.batch_size, n)
+            input_split = input[start:end]
+            labels.append(self.model(input_split).softmax(dim=1).argmax(dim=1))
+        labels = torch.cat(labels, dim=0)
+
+        per_pixel_accs = []
+        per_class_accs = []f
+        class_ious = []
+        for i in range(self.splits):
+            start = i     * n // self.splits
+            end   = (i+1) * n // self.splits
+            labels_split = labels[start:end]
+            per_pixel_accs.append(losses.per_pixel_acc(labels_split, target).item())
+            per_class_accs.append(losses.class_accs(labels_split, target).item())
+            class_ious.append(losses.class_ious(labels_split, target).item())
+
+        return np.mean(per_pixel_accs), np.std(per_pixel_accs), \
+                np.mean(class_accs), np.std(class_accs), \
+                np.mean(class_ious), np.std(class_ious)
 
 
 if __name__ == '__main__':
